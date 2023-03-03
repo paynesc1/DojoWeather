b4a9186dea683d416cadbf54339c1ceccff1c701
paynesc1 <paynesc1@gmail.com>
2023-03-02 18:00:22 -0700

    initial commit weather

---
+++ script.js
@@ -0,0 +1,20 @@
+function takeAway(element) {
+    //when button clicked
+    element.remove();
+    //remove entire message
+}
+
+function turnOn(e) {
+    e.classList.add("shadow")
+}
+
+function turnOff(e) {
+    e.classList.remove("shadow")
+}
+
+
+
+function change_temp(e) {
+    //when C selected 
+    //change values from F to C
+}
